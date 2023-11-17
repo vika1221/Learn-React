923cc3a58dff271ef01f9a08e9fe1c81c7dedffe
Vikash <vikashranjanofficial@gmail.com>
2023-11-17 10:16:48 +0530

    some changes are done

---
+++ 1st_day/app.js
@@ -0,0 +1,6 @@
+//  by javascript
+
+const heading=document.createElement("h1");
+heading.innerHTML="Hello Everyone from Javascript";
+const root=document.getElementById("root");
+root.appendChild(heading);
