f083feb60b62aef0eb8aedaa22613956003982ce
Vikash <vikashranjanofficial@gmail.com>
2023-11-17 11:07:44 +0530

    some changes are done

--- 1st_day/app.js
+++ 1st_day/app.js
@@ -1,6 +1,41 @@
 //  by javascript
 
-const heading=document.createElement("h1");
-heading.innerHTML="Hello Everyone from Javascript";
-const root=document.getElementById("root");
-root.appendChild(heading);
+// const heading=document.createElement("h1");
+// heading.innerHTML="Hello Everyone from Javascript";
+// const root=document.getElementById("root");
+// root.appendChild(heading);
+
+// by react js
+
+// const heading= React.createElement("h1",{},"Hello Everyone from React js");
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// More elements added
+
+const heading1=React.createElement(
+    "h1",
+    {
+        id:"title1"
+    },
+    "Heading 1"
+);
+
+const heading2=React.createElement(
+    "h2",
+    {
+        id:"title2"
+    },
+    "Heading 2"
+);
+
+const container=React.createElement(
+    "div",
+    {
+        id:"container"
+    },
+    [heading1,heading2]
+);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
